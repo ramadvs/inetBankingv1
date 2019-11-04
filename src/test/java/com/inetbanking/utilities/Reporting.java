7abe11c90da7b724837a6e655daada101d47714f
ramadvs <rsunkavalli18@gmail.com>
2019-11-03 22:22:07 -0800

    changed reporting file

--- src/test/java/com/inetbanking/utilities/Reporting.java
+++ src/test/java/com/inetbanking/utilities/Reporting.java
@@ -28,7 +28,7 @@ public void onStart(ITestContext testContext)
 {
 	String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
 	String repName = " Test-Report "+timeStamp+ ".html";
-	htmlReporter=new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-output"+repName);
+	htmlReporter=new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-output/"+repName);
 	htmlReporter.loadXMLConfig(System.getProperty("user.dir")+"/extent-config.xml");
 	
 	extent = new ExtentReports();
